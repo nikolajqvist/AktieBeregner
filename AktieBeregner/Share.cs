b593b4694ea209d02485e5d6d112c0ec2f32ebf2
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2025-02-20 13:19:33 +0100

    Add project files.

---
+++ AktieBeregner/Share.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AktieBeregner
+{
+    /// <summary>
+    /// Aktie klasse.
+    /// </summary>
+    public class Share
+    {
+        /// <summary>
+        /// Navnet på aktien
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Antal aktier
+        /// </summary>
+        public int Amount { get; set; }
+        /// <summary>
+        /// Salgspris på aktien
+        /// </summary>
+        public int SellPrice { get; set; }
+        /// <summary>
+        /// købs prisen på aktien
+        /// </summary>
+        public int BuyPrice { get; set; }
+        /// <summary>
+        /// Valutaen af aktie kurs
+        /// </summary>
+        public int Currency { get; set; }
+        /// <summary>
+        /// Construktør som aktien bliver født
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="amount"></param>
+        /// <param name="buyprice"></param>
+        /// <param name="sellprice"></param>
+        public Share(string name, int amount, int buyprice, int sellprice)
+        {
+            Name = name;
+            Amount = amount;
+            BuyPrice = buyprice;
+            SellPrice = sellprice;
+        }
+        /// <summary>
+        /// Udregning af afkastet af 
+        /// </summary>
+        /// <returns></returns>
+        public int ReturnValue()
+        {
+            return (BuyPrice - SellPrice) * Amount;
+        }
+    }
+}
